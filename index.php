3c8f92f068c28d7eddc3fe7c5db70649e246b457
gioimtg2003 <conggioi.pro264@gmail.com>
2023-05-25 21:32:55 +0700

    update

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php 
+$title = "Trang chủ";
+$_pathCss = "css/product/styles.css";
+require_once __DIR__ . "/views/Menu/dbProcess.php";
+require_once __DIR__ . "/inc/head.php";
+require_once __DIR__ . "/views/About/index.php";
+require_once __DIR__ . "/views/Menu/index.php";
+?>
+
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$timeCreate = date("Y-m-d H:i:s");
+echo var_dump($timeCreate);
+ ?>
